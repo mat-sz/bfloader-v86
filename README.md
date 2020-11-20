b9917f03af785cf4c23ecaed797d03da0bf7d84f
Mat Sz <contact@matsz.dev>
2020-11-20 18:56:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# bfloader-v86
+
+An online demo version of [bfloader](https://github.com/mat-sz/bfloader).
+
+## Credits
+
+Without [v86](https://github.com/copy/v86) (a JavaScript x86 VM by [@copy](https://github.com/copy)) this project would not have been possible.
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import { V86Starter } from "v86";
+
+import "./App.scss";
+
+import seabios from "./bios/seabios.bin";
+import vgabios from "./bios/vgabios.bin";
+import bfloader from "./img/bfloader.img";
+
+let loading = document.getElementById("loading");
+
+let emulator = new V86Starter({
+  screen_container: document.getElementById("screen_container"),
+  bios: {
+    url: seabios,
+  },
+  vga_bios: {
+    url: vgabios,
+  },
+  fda: {
+    url: bfloader,
+  },
+  autostart: true,
+});
+
+emulator.add_listener("screen-set-mode", (graphic) => {
+  if (graphic) {
+    loading.style.display = "none";
+  }
+});
